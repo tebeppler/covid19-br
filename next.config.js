095536b3f8cdc411c746a8c101cf41eb5a438cb6
Bernardo <bernaferrari2@gmail.com>
2020-04-21 16:26:55 -0300

    Fix a file.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   webpack: (config, options) => {
     config.resolve.extensions = ['.ts', '.tsx', '.js']
-    
-    config.module.rules.push({
-      test: /\.(png|jpg|gif|json)$/i,
-      loader: "url-loader?limit=100000",
-    });
 
     return config;
   },
